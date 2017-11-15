21cee3f4e2bd1f1f3a7eb0e164aa66bfb8209f65
tiagosb <tdsb.contato@gmail.com>
2017-11-14 21:39:53 -0300

    Remoção de bugs

--- PyCalc.py
+++ PyCalc.py
@@ -11,9 +11,7 @@ class Calc():
         self.parent = parent
         self.parent.title("PyCalc - Calculadora")
         self.parent.option_add("*Font", "Verdana 12 normal")
-        self.parent.iconbitmap("icones/calc_icon.ico")
-        #variavel de controle, utilizada no metodo key_press
-        self.decimal_point_open = False
+        self.parent.iconbitmap("icones/calc_icon.ico")        
         #variável contendo a expressão pronta para calculo
         self.math_expression = None
         #menubar
@@ -24,7 +22,7 @@ class Calc():
         self.display_stringvar = tk.StringVar()
         display_entry_validate = (self.parent.register(self.only_number_entry), '%S', '%d')
         self.display = tk.Entry(self.parent, font=("Verdana", 20, "normal"), validate="key", validatecommand=display_entry_validate, textvariable=self.display_stringvar)
-        self.display.bind('<Return>', self.calc_expression)
+        self.display.bind('<Return>', self.command_calc)
         #criação do teclado numerico
         self.bt0 = tk.Button(self.parent, text="0", command=lambda: self.button_press('0'))
         self.bt1 = tk.Button(self.parent, text="1", command=lambda: self.button_press('1'))
@@ -41,10 +39,10 @@ class Calc():
         self.bt_mutiplication = tk.Button(self.parent, text="*", command=lambda: self.button_press('*'))
         self.bt_division = tk.Button(self.parent, text="/", command=lambda: self.button_press('/'))
         self.bt_decimal_point = tk.Button(self.parent, text=".", command=lambda: self.button_press('.'))
-        self.bt_equal = tk.Button(self.parent, text="=", command=lambda: self.button_press('='))
+        self.bt_equal = tk.Button(self.parent, text="=", command=lambda: self.button_press("="))
         self.bt_clear = tk.Button(self.parent, text="Limpar", command=self.clear)
-        self.bt_quit = tk.Button(self.parent, text="Sair(Esc)", command=self.parent.destroy)
-        self.bt_delete = tk.Button(self.parent, text="Del", command=self.delete)
+        self.bt_quit = tk.Button(self.parent, text="Sair", command=self.parent.destroy)
+        self.bt_delete = tk.Button(self.parent, text="Del", command=lambda: self.button_press('Delete'))
         self.parent.bind("<Escape>", lambda event=None: self.parent.destroy())
     
     def run(self):
@@ -71,24 +69,13 @@ class Calc():
         self.bt_decimal_point.grid(row=4, column=1, sticky="ewns", padx=2, pady=2)
         self.bt_equal.grid(row=4, column=2, sticky="ewns", padx=2, pady=2)
         self.bt_delete.grid(row=5, column=3, sticky="wens", padx=2, pady=2)
-        self.bt_clear.grid(row=5, column=2,sticky="ewns", padx=2, pady=2)
-        self.bt_quit.grid(row=5, column=0,columnspan=2,sticky="ewns", padx=2, pady=2)
+        self.bt_clear.grid(row=5, column=1,columnspan=2,sticky="ewns", padx=2, pady=2)
+        self.bt_quit.grid(row=5, column=0,sticky="ewns", padx=2, pady=2)
 
     #limpa o display    
     def clear(self):
         self.display_stringvar.set("")
-        self.decimal_point_open = False
-    
-    #apaga o ultimo elemento
-    def delete(self):
-        str_now = self.display_stringvar.get()
-        str_size = len(str_now)
-        new_string = ""        
-        if str_size > 0:
-            for index in range(0,len(str_now)):
-                if not index == str_size-1:
-                    new_string += str_now[index]
-            self.display_stringvar.set(new_string)
+        self.math_expression = None
             
     #tela sobre
     def about(self):
@@ -101,109 +88,100 @@ class Calc():
         tk.Label(about_window, text="tdsb.contato@gmail.com").grid(row=3, column=0, sticky="s", padx=5)
 
     #validador de entrys somente expressões matemáticas válidas
-    def only_number_entry(self, key_press, cod):
-        # valid_chars = "0123456789.+-*/"
-        expression_now = self.display_stringvar.get() 
-        num_chars_now = len(expression_now)
-        #no inicio da expressão, aceita apenas numeros
-        if num_chars_now == 0:
-            valid_chars_for_init = "0123456789"
-            return key_press in valid_chars_for_init        
+    def only_number_entry(self, button, operation):
+        isvalid = False
+        numbers = "0123456789"
+        operators = "+-*/" 
+        string = self.display_stringvar.get()
+        index = len(string)
+        str_size = index
+        #permite remoção
+        if operation == '0':
+            index-=1
+            isvalid = True
+        #primeira posição aceita apenas numeros
+        if str_size == 0:
+            isvalid = button in numbers
         else:
-            last_char = expression_now[num_chars_now-1]
-            #se o ultimo elemento inserido é um operador aceita apenas numeros ou backspace
-            if last_char in "+-*/" and key_press in "+-*/." and cod == '1':
-                return False
-            #controla o uso de ponto decimal    
-            #evita dois operadores seguidos    
-            elif last_char in "+-*/" and key_press in "+-*/." and cod == '0':
-                return True
-            elif last_char == '.' and key_press in "+-*/." and cod == '1':
-                return False
-            elif last_char == '.' and key_press in "+-*/." and cod == '0':
-                return True
-            elif self.decimal_point_open and key_press == '.':
-                return False
-            elif not self.decimal_point_open and key_press == '.':
-                self.decimal_point_open = True
-                return True
-            elif last_char in "0123456789" and key_press in "+-*/":
-                self.decimal_point_open = False
-                return True
-        
-    #botão da interface pressionado
-    def button_press(self, bt):
-        expression_now = self.display_stringvar.get()
-        char_num = len(expression_now)
-        #define caracteres validos para o inicio da expressão
-        if char_num == 0:
-            valid_chars_for_init = "0123456789"
-            if bt in valid_chars_for_init:
-                expression_now += bt
-                self.display_stringvar.set(expression_now)
-        else:
-            #controle do uso de ponto decimal
-            last_char = expression_now[char_num - 1]
-            if bt == '.' and not last_char in "+-*/":
-                if self.decimal_point_open:
-                    print("ponto decimal indisponivel")
-                else:
-                    expression_now += bt
-                    self.display_stringvar.set(expression_now)
-                    self.decimal_point_open = True
-            else:
-                if last_char in ".0123456789" and bt in "0123456789":
-                    expression_now += bt
-                    self.display_stringvar.set(expression_now)
-                elif last_char in "0123456789" and not self.decimal_point_open and bt in "+-*/":
-                    expression_now += bt
-                    self.display_stringvar.set(expression_now)
-                elif self.decimal_point_open and not last_char == '.' and bt in "+-*/":
-                    expression_now += bt
-                    self.display_stringvar.set(expression_now)
-                    self.decimal_point_open = False
-                #controle do uso de operadores
-                elif last_char in "+-*/" and bt in "0123456789":
-                    expression_now += bt
-                    self.display_stringvar.set(expression_now)
-                #botão = pressionado, obter resultado
-                elif bt == '=' and last_char in "0123456789":
-                    #calcula a expressão
-                    self.calc_expression()
+            #pega o ultimo caractere na string
+            last_char = string[index-1]
+            #se ultimo caracter é numero e o pressionado é numero, operador ou ponto
+            if last_char in numbers and button in numbers or last_char in numbers and button in operators or last_char in numbers and button  == ".":
+                isvalid = True
+            elif last_char == "." and button in numbers:
+                isvalid = True
+            elif last_char in operators and button in numbers:
+                isvalid = True 
+        return isvalid
 
+    #botão da interface pressionado
+    def button_press(self, button):
+        index = len(self.display_stringvar.get())
+        if button == "Delete":
+            index-=1
+            self.display.delete(index,'end')
+        elif button == "=" and self.expression_check():         
+            self.calc_expression()
+        else:   
+            self.display.insert(index, button)
+    
+    #enter no display equivalente ao '='
+    def command_calc(self,event=None):
+        if self.expression_check():
+            self.calc_expression()
+
+    #antes de começar o calculo, checa se o final da expressão é válida
+    def expression_check(self):
+        isvalid = False
+        string = self.display_stringvar.get()
+        index = len(string)
+        if index == 0:
+            isvalid = False
+        else:
+            index -= 1
+            last_char = string[index]   
+            if last_char in "+-*/.":
+                isvalid = False
+            elif last_char in "0123456789":
+                isvalid = True
+        return isvalid
+
+    #prepara a expressão para o calculo
     def prepare_expression(self):
-        elementos = []
+        elements = []
         index = 0
+        operators = "+-*/"
         for char in self.display_stringvar.get():
             #inicializa a lista com o primeiro numero
-            if len(elementos) == 0 and char in "0123456789":
-                elementos.append(char)
+            if len(elements) == 0 and char in "0123456789":
+                elements.append(char)
             #novo operador como novo elemento da lista
-            elif len(elementos) > 0 and char in "+-*/":
-                elementos.append(char)
+            elif len(elements) > 0 and char in operators:
+                elements.append(char)
                 index += 1
-            elif elementos[index] in "+-*/":
-                elementos.append(char)
+            elif elements[index] in operators:
+                elements.append(char)
                 index += 1
             else:
-                elementos[index] += char
+                elements[index] += char
         #salva a expressão preparada como atributo do objeto
-        self.math_expression = elementos
+        self.math_expression = elements
 
     #efetua o calculo da expressão
-    def calc_expression(self):
+    def calc_expression(self, event=None):
         self.prepare_expression()
-        # multiplicação e divisão, precedencia: o que vier primeiro da esquerda para a direita
+        #multiplicação e divisão
+        #precedencia: o que vier primeiro da esquerda para a direita
         while "*" in self.math_expression or "/" in self.math_expression:
             index = 0
             for element in self.math_expression:
                 if element in '/*':
-                    v1 = float(self.math_expression[index - 1])
-                    v2 = float(self.math_expression[index + 1])
+                    value1 = float(self.math_expression[index - 1])
+                    value2 = float(self.math_expression[index + 1])
                     if element == '*':
-                        result = str(v1 * v2)
+                        result = str(value1 * value2)
                     elif element == '/':
-                        result = str(v1 / v2)
+                        result = str(value1 / value2)
                     self.math_expression[index] = result
                     self.math_expression.pop(index + 1)
                     self.math_expression.pop(index - 1)
@@ -215,19 +193,21 @@ class Calc():
             index = 0
             for element in self.math_expression:
                 if element in '+-':
-                    v1 = float(self.math_expression[index - 1])
-                    v2 = float(self.math_expression[index + 1])
-                if element == '+':
-                    result = str(v1 + v2)
-                if element == '-':
-                    result = str(v1 - v2)
-                if element in '-+':
+                    value1 = float(self.math_expression[index - 1])
+                    value2 = float(self.math_expression[index + 1])
+                    if element == '+':
+                        result = str(value1 + value2)
+                    elif element == '-':
+                        result = str(value1 - value2)
                     self.math_expression[index] = result
                     self.math_expression.pop(index + 1)
                     self.math_expression.pop(index - 1)
                     break
                 index += 1
-        final_result = str(round(float(self.math_expression[0]), 1))
+        final_result = round(float(self.math_expression[0]), 1)
+        if final_result % 1 == 0:
+            final_result = int(final_result)
+        final_result = str(final_result)
         #insere o resultado no display
         self.display_stringvar.set(final_result)
 
