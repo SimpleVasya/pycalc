9f8c6d3040ad9cf63a567bd6baada561dace652a
tiagosb <tdsb.contato@gmail.com>
2018-07-25 08:09:45 -0300

    Refatora o codigo de criacao e posicionamento dos botoes

--- PyCalc.py
+++ PyCalc.py
@@ -18,31 +18,22 @@ class Calc():
         menu_bar = tk.Menu(self.parent)
         menu_bar.add_command(label="Sobre", command=self.about)
         self.parent.config(menu=menu_bar)
+        
         #elementos do display
         self.display_stringvar = tk.StringVar()
         display_entry_validate = (self.parent.register(self.only_number_entry), '%S', '%d')
         self.display = tk.Entry(self.parent, font=("Verdana", 20, "normal"), validate="key", validatecommand=display_entry_validate, textvariable=self.display_stringvar)
         self.display.bind('<Return>', self.command_calc)
+        
         #criação do teclado numerico
-        self.bt0 = tk.Button(self.parent, text="0", command=lambda: self.button_press('0'))
-        self.bt1 = tk.Button(self.parent, text="1", command=lambda: self.button_press('1'))
-        self.bt2 = tk.Button(self.parent, text="2", command=lambda: self.button_press('2'))
-        self.bt3 = tk.Button(self.parent, text="3", command=lambda: self.button_press('3'))
-        self.bt4 = tk.Button(self.parent, text="4", command=lambda: self.button_press('4'))
-        self.bt5 = tk.Button(self.parent, text="5", command=lambda: self.button_press('5'))
-        self.bt6 = tk.Button(self.parent, text="6", command=lambda: self.button_press('6'))
-        self.bt7 = tk.Button(self.parent, text="7", command=lambda: self.button_press('7'))
-        self.bt8 = tk.Button(self.parent, text="8", command=lambda: self.button_press('8'))
-        self.bt9 = tk.Button(self.parent, text="9", command=lambda: self.button_press('9'))
-        self.bt_addition = tk.Button(self.parent, text="+", command=lambda: self.button_press('+'))
-        self.bt_subtraction = tk.Button(self.parent, text="-", command=lambda: self.button_press('-'))
-        self.bt_mutiplication = tk.Button(self.parent, text="*", command=lambda: self.button_press('*'))
-        self.bt_division = tk.Button(self.parent, text="/", command=lambda: self.button_press('/'))
-        self.bt_decimal_point = tk.Button(self.parent, text=".", command=lambda: self.button_press('.'))
-        self.bt_equal = tk.Button(self.parent, text="=", command=lambda: self.button_press("="))
-        self.bt_clear = tk.Button(self.parent, text="Limpar", command=self.clear)
-        self.bt_quit = tk.Button(self.parent, text="Sair", command=self.parent.destroy)
-        self.bt_delete = tk.Button(self.parent, text="Del", command=lambda: self.button_press('Delete'))
+        #Gera os botões de forma dinâmica
+        #refatorado 25/07/2018
+        self.buttons = []
+        simbols = ['7','8','9','*','4','5','6','-','1','2','3','+','0','.','=','/','Sair','Limpar','Del']
+        for simbol in simbols:
+            new_button = tk.Button(self.parent, text=simbol, command=lambda simbol=simbol:self.button_press(simbol))
+            self.buttons.append(new_button)
+
         self.parent.bind("<Escape>", lambda event=None: self.parent.destroy())
     
     def run(self):
@@ -51,26 +42,27 @@ class Calc():
 
     #monta e configura os componentes gráficos
     def mount_gui(self):
+        
         self.display.grid(row=0, column=0, columnspan=4, sticky="ewns", ipady=5, padx=2, pady=2)
-        self.bt0.grid(row=4, column=0, sticky="ewns", padx=2, pady=2)
-        self.bt1.grid(row=3, column=0, sticky="ewns", padx=2, pady=2)
-        self.bt2.grid(row=3, column=1, sticky="ewns", padx=2, pady=2)
-        self.bt3.grid(row=3, column=2, sticky="ewns", padx=2, pady=2)
-        self.bt4.grid(row=2, column=0, sticky="ewns", padx=2, pady=2)
-        self.bt5.grid(row=2, column=1, sticky="ewns", padx=2, pady=2)
-        self.bt6.grid(row=2, column=2, sticky="ewns", padx=2, pady=2)
-        self.bt7.grid(row=1, column=0, sticky="ewns", padx=2, pady=2)
-        self.bt8.grid(row=1, column=1, sticky="ewns", padx=2, pady=2)
-        self.bt9.grid(row=1, column=2, sticky="ewns", padx=2, pady=2)
-        self.bt_addition.grid(row=3, column=3, sticky="ewns", padx=2, pady=2)
-        self.bt_subtraction.grid(row=2, column=3, sticky="ewns", padx=2, pady=2)
-        self.bt_division.grid(row=4, column=3, sticky="ewns", padx=2, pady=2)
-        self.bt_mutiplication.grid(row=1, column=3, sticky="ewns", padx=2, pady=2)
-        self.bt_decimal_point.grid(row=4, column=1, sticky="ewns", padx=2, pady=2)
-        self.bt_equal.grid(row=4, column=2, sticky="ewns", padx=2, pady=2)
-        self.bt_delete.grid(row=5, column=3, sticky="wens", padx=2, pady=2)
-        self.bt_clear.grid(row=5, column=1,columnspan=2,sticky="ewns", padx=2, pady=2)
-        self.bt_quit.grid(row=5, column=0,sticky="ewns", padx=2, pady=2)
+        
+        #Posiciona os botões dinâmicamente
+        #Refatorado 25/07/2018
+        row, column = 1, 0
+        for button in self.buttons:
+            #A posição 5x2 não deve ser ocupada, Limpar irá ocupa-la 
+            if row==5 and column==2:
+                column+=1
+            #Posiciona o Limpar em 5x1
+            if row==5 and column==1:
+                button.grid(row=row, column=column, columnspan=2,sticky="ewns", padx=2, pady=2)
+            else:
+                button.grid(row=row, column=column,sticky="ewns", padx=2, pady=2)
+
+            if column == 3:
+                column = 0
+                row+=1
+            else:
+                column+=1
 
     #limpa o display    
     def clear(self):
@@ -117,9 +109,13 @@ class Calc():
     #botão da interface pressionado
     def button_press(self, button):
         index = len(self.display_stringvar.get())
-        if button == "Delete":
+        if button == "Del":
             index-=1
             self.display.delete(index,'end')
+        elif button == 'Limpar':
+            self.clear()
+        elif button == 'Sair':
+            self.parent.destroy()
         elif button == "=" and self.expression_check():         
             self.calc_expression()
         else:   
@@ -155,7 +151,7 @@ class Calc():
             #inicializa a lista com o primeiro numero
             if len(elements) == 0 and char in "0123456789":
                 elements.append(char)
-            #novo operador como novo elemento da lista
+            #operador como novo elemento da lista
             elif len(elements) > 0 and char in operators:
                 elements.append(char)
                 index += 1
